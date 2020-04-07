b45081741b615bbea734050306815cc62d2fa0cb
Oliver <xumeng0515@qq.com>
2020-04-07 10:23:26 +0800

    添加开机自启

--- VolumnUtility/SystemVolumn.cs
+++ VolumnUtility/SystemVolumn.cs
@@ -5,10 +5,6 @@ namespace VolumnUtility
 {
     public static class SystemVolume
     {
-        #region Master Volume Manipulation
-        /// <summary>
-        /// 返回系统音量(1~100)
-        /// </summary>
         public static float GetMasterVolume()
         {
             IAudioEndpointVolume masterVol = null;
@@ -28,9 +24,6 @@ namespace VolumnUtility
             }
         }
 
-        /// <summary>
-        /// 设置系统音量
-        /// </summary>
         public static void SetMasterVolume(float newLevel)
         {
             IAudioEndpointVolume masterVol = null;
@@ -49,10 +42,6 @@ namespace VolumnUtility
             }
         }
 
-        /// <summary>
-        /// 设置系统静音
-        /// </summary>
-        /// <param name="isMuted"></param>
         public static void SetMasterVolumeMute(bool isMuted)
         {
             IAudioEndpointVolume masterVol = null;
@@ -92,13 +81,8 @@ namespace VolumnUtility
                 if (deviceEnumerator != null) Marshal.ReleaseComObject(deviceEnumerator);
             }
         }
-
-        #endregion
     }
 
-
-
-    #region Abstracted COM interfaces from Windows CoreAudio API
     [ComImport]
     [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
     internal class MMDeviceEnumerator
@@ -128,8 +112,6 @@ namespace VolumnUtility
 
         [PreserveSig]
         int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppDevice);
-
-        // the rest is not implemented
     }
 
     [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
@@ -137,8 +119,6 @@ namespace VolumnUtility
     {
         [PreserveSig]
         int Activate(ref Guid iid, int dwClsCtx, IntPtr pActivationParams, [MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
-
-        // the rest is not implemented
     }
 
 
@@ -156,7 +136,6 @@ namespace VolumnUtility
     [Guid("bfb7ff88-7239-4fc9-8fa2-07c950be9c6d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     internal interface IAudioSessionControl2
     {
-        // IAudioSessionControl
         [PreserveSig]
         int NotImpl0();
 
@@ -184,7 +163,6 @@ namespace VolumnUtility
         [PreserveSig]
         int NotImpl2();
 
-        // IAudioSessionControl2
         [PreserveSig]
         int GetSessionIdentifier([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
 
@@ -210,173 +188,80 @@ namespace VolumnUtility
         [PreserveSig]
         int NotImpl2();
 
-        /// <summary>
-        /// Gets a count of the channels in the audio stream.
-        /// </summary>
-        /// <param name="channelCount">The number of channels.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetChannelCount(
         [Out] [MarshalAs(UnmanagedType.U4)] out UInt32 channelCount);
 
-        /// <summary>
-        /// Sets the master volume level of the audio stream, in decibels.
-        /// </summary>
-        /// <param name="level">The new master volume level in decibels.</param>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int SetMasterVolumeLevel(
         [In] [MarshalAs(UnmanagedType.R4)] float level,
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Sets the master volume level, expressed as a normalized, audio-tapered value.
-        /// </summary>
-        /// <param name="level">The new master volume level expressed as a normalized value between 0.0 and 1.0.</param>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int SetMasterVolumeLevelScalar(
         [In] [MarshalAs(UnmanagedType.R4)] float level,
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Gets the master volume level of the audio stream, in decibels.
-        /// </summary>
-        /// <param name="level">The volume level in decibels.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetMasterVolumeLevel(
         [Out] [MarshalAs(UnmanagedType.R4)] out float level);
 
-        /// <summary>
-        /// Gets the master volume level, expressed as a normalized, audio-tapered value.
-        /// </summary>
-        /// <param name="level">The volume level expressed as a normalized value between 0.0 and 1.0.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetMasterVolumeLevelScalar(
         [Out] [MarshalAs(UnmanagedType.R4)] out float level);
 
-        /// <summary>
-        /// Sets the volume level, in decibels, of the specified channel of the audio stream.
-        /// </summary>
-        /// <param name="channelNumber">The channel number.</param>
-        /// <param name="level">The new volume level in decibels.</param>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int SetChannelVolumeLevel(
         [In] [MarshalAs(UnmanagedType.U4)] UInt32 channelNumber,
         [In] [MarshalAs(UnmanagedType.R4)] float level,
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Sets the normalized, audio-tapered volume level of the specified channel in the audio stream.
-        /// </summary>
-        /// <param name="channelNumber">The channel number.</param>
-        /// <param name="level">The new master volume level expressed as a normalized value between 0.0 and 1.0.</param>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int SetChannelVolumeLevelScalar(
         [In] [MarshalAs(UnmanagedType.U4)] UInt32 channelNumber,
         [In] [MarshalAs(UnmanagedType.R4)] float level,
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Gets the volume level, in decibels, of the specified channel in the audio stream.
-        /// </summary>
-        /// <param name="channelNumber">The zero-based channel number.</param>
-        /// <param name="level">The volume level in decibels.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetChannelVolumeLevel(
         [In] [MarshalAs(UnmanagedType.U4)] UInt32 channelNumber,
         [Out] [MarshalAs(UnmanagedType.R4)] out float level);
 
-        /// <summary>
-        /// Gets the normalized, audio-tapered volume level of the specified channel of the audio stream.
-        /// </summary>
-        /// <param name="channelNumber">The zero-based channel number.</param>
-        /// <param name="level">The volume level expressed as a normalized value between 0.0 and 1.0.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetChannelVolumeLevelScalar(
         [In] [MarshalAs(UnmanagedType.U4)] UInt32 channelNumber,
         [Out] [MarshalAs(UnmanagedType.R4)] out float level);
 
-        /// <summary>
-        /// Sets the muting state of the audio stream.
-        /// </summary>
-        /// <param name="isMuted">True to mute the stream, or false to unmute the stream.</param>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int SetMute(
         [In] [MarshalAs(UnmanagedType.Bool)] Boolean isMuted,
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Gets the muting state of the audio stream.
-        /// </summary>
-        /// <param name="isMuted">The muting state. True if the stream is muted, false otherwise.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetMute(
         [Out] [MarshalAs(UnmanagedType.Bool)] out Boolean isMuted);
 
-        /// <summary>
-        /// Gets information about the current step in the volume range.
-        /// </summary>
-        /// <param name="step">The current zero-based step index.</param>
-        /// <param name="stepCount">The total number of steps in the volume range.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetVolumeStepInfo(
         [Out] [MarshalAs(UnmanagedType.U4)] out UInt32 step,
         [Out] [MarshalAs(UnmanagedType.U4)] out UInt32 stepCount);
 
-        /// <summary>
-        /// Increases the volume level by one step.
-        /// </summary>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int VolumeStepUp(
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Decreases the volume level by one step.
-        /// </summary>
-        /// <param name="eventContext">A user context value that is passed to the notification callback.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int VolumeStepDown(
         [In] [MarshalAs(UnmanagedType.LPStruct)] Guid eventContext);
 
-        /// <summary>
-        /// Queries the audio endpoint device for its hardware-supported functions.
-        /// </summary>
-        /// <param name="hardwareSupportMask">A hardware support mask that indicates the capabilities of the endpoint.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int QueryHardwareSupport(
         [Out] [MarshalAs(UnmanagedType.U4)] out UInt32 hardwareSupportMask);
 
-        /// <summary>
-        /// Gets the volume range of the audio stream, in decibels.
-        /// </summary>
-        /// <param name="volumeMin">The minimum volume level in decibels.</param>
-        /// <param name="volumeMax">The maximum volume level in decibels.</param>
-        /// <param name="volumeStep">The volume increment level in decibels.</param>
-        /// <returns>An HRESULT code indicating whether the operation passed of failed.</returns>
         [PreserveSig]
         int GetVolumeRange(
         [Out] [MarshalAs(UnmanagedType.R4)] out float volumeMin,
         [Out] [MarshalAs(UnmanagedType.R4)] out float volumeMax,
         [Out] [MarshalAs(UnmanagedType.R4)] out float volumeStep);
     }
-    #endregion
 }
